2f8c883e721031847772e018ba68468f669b9c48
wilian santos <14809032647@estudante.ufjf.br>
2023-05-30 20:58:53 -0300

    adiona o remover

--- store.js
+++ store.js
@@ -6,7 +6,10 @@ export function adiciona (valor)
     store.estado.push(valor);
 
 }
-
+export function remover (valor)
+{
+    store.estado.pop(valor);
+}
 export function getitems ()
 {
     return [...store.estado];
